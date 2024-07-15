1b36387610e58b3ec64c4037169a2b073cde5732
David Arenas <davejs136@gmail.com>
2024-07-15 13:45:58 -0400

    feat: add real data in products index page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
+import type { Strapi4ResponseMany } from "@nuxtjs/strapi/dist/runtime/types";
+import type { ProductAttributes } from "~/types/app";
+
+const { find } = useStrapi();
+
+const { data: productsResponse, suspense: productsSuspense } = useQuery({
+  queryKey: ["products"],
+  queryFn: () =>
+    find("products", {
+      populate: "*",
+    }) as unknown as Strapi4ResponseMany<ProductAttributes>,
+  select(data) {
+    return data;
+  },
+  staleTime: 1000 * 60 * 5, // 5 minutes
+});
+
+const products = computed(() => productsResponse.value?.data);
+
+await productsSuspense();
+</script>
+
 <template>
   <section class="page">
     <!-- ***** Page Top Start ***** -->
@@ -6,14 +30,14 @@
         <div class="container">
           <div class="row">
             <div class="offset-lg-3 col-lg-6">
-              <h1>Projects</h1>
+              <h1>Products</h1>
             </div>
           </div>
           <div class="row">
             <div class="col-lg-12">
               <ol class="breadcrumb">
-                <li><a href="index.html">Home</a></li>
-                <li class="active">Projects</li>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li class="active">Products</li>
               </ol>
             </div>
           </div>
@@ -29,47 +53,35 @@
           <div class="col-lg-12">
             <div class="project-grid">
               <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                  <a href="project-single.html" class="project-grid-item">
+                <div
+                  v-for="product in products"
+                  :key="product.attributes.slug"
+                  class="col-lg-4 col-md-6 col-sm-12"
+                >
+                  <nuxt-link
+                    class="project-grid-item"
+                    :to="`/products/${product.attributes.category.data.attributes.slug}/${product.attributes.slug}`"
+                  >
                     <div class="img">
-                      <img src="assets/images/photos/project/1.jpg" alt="" />
+                      <img
+                        :src="
+                          getImageUrl(
+                            product.attributes.image.data.attributes.url
+                          )
+                        "
+                        :alt="
+                          product.attributes.image.data.attributes
+                            ?.alternativeText || 'image'
+                        "
+                      />
                     </div>
                     <div class="text">
-                      <h3>MODERN KITCHENS</h3>
-                      <p>
-                        Proin luctus odio et purus iaculis, et porta ex
-                        molestie. Curabitur euismod nulla enim.
-                      </p>
+                      <h3>{{ product.attributes.title }}</h3>
+                      <!-- <p>
+                        {{ product.attributes.description }}
+                      </p> -->
                     </div>
-                  </a>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                  <a href="project-single.html" class="project-grid-item">
-                    <div class="img">
-                      <img src="assets/images/photos/project/2.jpg" alt="" />
-                    </div>
-                    <div class="text">
-                      <h3>MODERN BATHROOM</h3>
-                      <p>
-                        Curabitur nec imperdiet elit. Ut non erat imperdiet,
-                        condi men tum turpis nec, sceleris que.
-                      </p>
-                    </div>
-                  </a>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                  <a href="project-single.html" class="project-grid-item">
-                    <div class="img">
-                      <img src="assets/images/photos/project/3.jpg" alt="" />
-                    </div>
-                    <div class="text">
-                      <h3>DECORATIVE CHAIR</h3>
-                      <p>
-                        Aliquam at eros vitae elit vulputate venenatis sed id
-                        augue. Nuncmi nisl, pulvinar.
-                      </p>
-                    </div>
-                  </a>
+                  </nuxt-link>
                 </div>
               </div>
             </div>
